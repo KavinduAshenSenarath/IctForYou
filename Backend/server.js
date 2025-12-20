607e12b5188db38415ce8e236b6c03fdb4262ed1
kavinduAshen <kavinduashensenarath@gmail.com>
2025-12-20 23:28:37 +0530

    feat:create user model and user routes

--- Backend/server.js
+++ Backend/server.js
@@ -1,6 +1,7 @@
 import express from "express";
 import dotenv from "dotenv";
 import connectDB from "./Config/db.js";
+import userRoutes from "./routes/userRoutes.js";
 
 dotenv.config();
 
@@ -13,6 +14,7 @@ connectDB();
 app.get("/", (req, res) => {
     res.send("Server is running");
 });
+app.use("/api/users", userRoutes);
 
 const PORT = process.env.PORT || 5000;
 
